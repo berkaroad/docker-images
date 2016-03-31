b7df3fde5b22c0db61be3e784163a0b8f63b430a
Jerry Bai <chinaren_bjr@163.com>
2016-03-31 16:33:20 +0800

    采用 gosu来模拟用户

--- redis/docker-entrypoint.sh
+++ redis/docker-entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 set -e
+sysctl vm.overcommit_memory=1 \
+        && echo never > /sys/kernel/mm/transparent_hugepage/enabled \
+        && echo 512 > /proc/sys/net/core/somaxconn
+
+if [ "$1" = 'redis-server' ]; then
+	chown -R redis .
+	exec gosu redis "$@"
+fi
 
 exec "$@"
