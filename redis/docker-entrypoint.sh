f81d7e3443517793caa6cf0ef5754531ac11d4e1
Jerry Bai <chinaren_bjr@163.com>
2016-04-11 14:10:23 +0800

    redis sysctl操作，仅在运行redis-server时执行

--- redis/docker-entrypoint.sh
+++ redis/docker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
-sysctl vm.overcommit_memory=1 \
-        && echo never > /sys/kernel/mm/transparent_hugepage/enabled \
-        && echo 512 > /proc/sys/net/core/somaxconn
 
 if [ "$1" = 'redis-server' ]; then
+	sysctl vm.overcommit_memory=1 \
+        && echo never > /sys/kernel/mm/transparent_hugepage/enabled \
+        && echo 512 > /proc/sys/net/core/somaxconn
 	chown -R redis .
 	exec gosu redis "$@"
 fi
