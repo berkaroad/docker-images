71755995fb142ec6e72325850468ca48f8356141
Jerry Bai <chinaren_bjr@163.com>
2016-05-26 15:19:03 +0800

    debian bug修复，解决不能自定义supervisor的问题

--- debian/docker-entrypoint.sh
+++ debian/docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-ls /supervisor/*.conf 2>dev/null | grep conf | xargs -i cp {} /etc/supervisor/conf.d
+ls /supervisor/*.conf 2>/dev/null | grep conf | xargs -i cp {} /etc/supervisor/conf.d
 exec "$@"
