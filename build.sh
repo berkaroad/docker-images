fe8a80edbdb7bdc34094e18f7a8f665f684bc3e4
Jerry Bai <chinaren_bjr@163.com>
2016-05-29 21:42:18 +0800

    修正build.sh

--- build.sh
+++ build.sh
@@ -1,38 +1,44 @@
 #!/bin/sh
-DF_ROOT=`pwd`
+# Build all docker images.
 
-cd $DF_ROOT/debian
-chmod +x ./build.sh && ./build.sh
+if [ -f $0 ] && [ "$0" = "./build.sh" ]; then
+    DF_ROOT=`pwd`
 
-cd $DF_ROOT/debian-ssh
-chmod +x ./build.sh && ./build.sh
+    cd $DF_ROOT/debian
+    chmod +x ./build.sh && ./build.sh
 
-cd $DF_ROOT/haproxy
-chmod +x ./build.sh && ./build.sh
+    cd $DF_ROOT/debian-ssh
+    chmod +x ./build.sh && ./build.sh
 
-cd $DF_ROOT/redis
-chmod +x ./build.sh && ./build.sh
+    cd $DF_ROOT/haproxy
+    chmod +x ./build.sh && ./build.sh
 
-cd $DF_ROOT/java
-chmod +x ./build.sh && ./build.sh
+    cd $DF_ROOT/redis
+    chmod +x ./build.sh && ./build.sh
 
-cd $DF_ROOT/selenium-base
-chmod +x ./build.sh && ./build.sh
+    cd $DF_ROOT/java
+    chmod +x ./build.sh && ./build.sh
 
-cd $DF_ROOT/selenium-nodebase
-chmod +x ./build.sh && ./build.sh
+    cd $DF_ROOT/selenium-base
+    chmod +x ./build.sh && ./build.sh
 
-cd $DF_ROOT/selenium-hub
-chmod +x ./build.sh && ./build.sh
+    cd $DF_ROOT/selenium-nodebase
+    chmod +x ./build.sh && ./build.sh
 
-cd $DF_ROOT/selenium-nodechrome
-chmod +x ./build.sh && ./build.sh
+    cd $DF_ROOT/selenium-hub
+    chmod +x ./build.sh && ./build.sh
 
-cd $DF_ROOT/elasticsearch
-chmod +x ./build.sh && ./build.sh
+    cd $DF_ROOT/selenium-nodechrome
+    chmod +x ./build.sh && ./build.sh
 
-cd $DF_ROOT/logstash
-chmod +x ./build.sh && ./build.sh
+    cd $DF_ROOT/elasticsearch
+    chmod +x ./build.sh && ./build.sh
+
+    cd $DF_ROOT/logstash
+    chmod +x ./build.sh && ./build.sh
 
 
-cd $DF_ROOT
+    cd $DF_ROOT
+else
+    echo "Please run like this: \"./build.sh\"."
+fi
