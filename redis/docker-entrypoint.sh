5bdda91cb38ed07cb4bf0daa2a8ab74fb637e837
Jerry Bai <chinaren_bjr@163.com>
2016-07-05 11:45:57 +0800

    redis 配置文件通过 supervisor来指定

--- redis/docker-entrypoint.sh
+++ redis/docker-entrypoint.sh
@@ -5,4 +5,4 @@ sysctl vm.overcommit_memory=1
 echo never > /sys/kernel/mm/transparent_hugepage/enabled
 echo 512 > /proc/sys/net/core/somaxconn
 chown -R redis /data
-exec gosu redis redis-server /redis.conf
+exec gosu redis redis-server $@
