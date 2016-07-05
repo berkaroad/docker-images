4d41ca8627a714eb8c218a9decc8584a32f491a8
Jerry Bai <chinaren_bjr@163.com>
2016-07-05 12:13:18 +0800

    redis 去掉需要privilege的代码，确保宿主的安全性

--- redis/docker-entrypoint.sh
+++ redis/docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-sysctl vm.overcommit_memory=1
-echo never > /sys/kernel/mm/transparent_hugepage/enabled
-echo 512 > /proc/sys/net/core/somaxconn
+#sysctl vm.overcommit_memory=1
+#echo never > /sys/kernel/mm/transparent_hugepage/enabled
+#echo 512 > /proc/sys/net/core/somaxconn
 chown -R redis /data
 exec gosu redis redis-server $@
