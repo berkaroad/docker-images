38568ee4095ccc4346150cb95f7cfbb742f4d411
Jerry Bai <chinaren_bjr@163.com>
2016-08-01 17:40:27 +0800

    elasticsearch  remove backup chown
    elasticsearch backup directory recommended to use shared storage, such
    as nfs

--- elasticsearch/docker-entrypoint.sh
+++ elasticsearch/docker-entrypoint.sh
@@ -3,5 +3,4 @@ set -e
 chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
 chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/config
 chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/logs
-chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/backups
 gosu elasticsearch /usr/share/elasticsearch/bin/elasticsearch
